7a271954f401dae9411acac9c1c9ddd369a91c83
xuningh2 <xuningh2@illinois.edu>
2019-12-07 22:03:46 -0600

    developer info fixed

--- lib/developer_info_button.dart
+++ lib/developer_info_button.dart
@@ -20,7 +20,7 @@ class DeveloperInfoButtonState extends State<DeveloperInfoButtonWidget> {
             new Icon(Icons.person),
             new FlatButton(
               onPressed: () {
-                developerInfoDialog();
+                openDialog();
               },
               child: 
               Text('Developer Info',
@@ -32,37 +32,36 @@ class DeveloperInfoButtonState extends State<DeveloperInfoButtonWidget> {
       ],
     );
   }
-  void developerInfoDialog() {
-    Future<void> OpenDialog() async {
-      switch (await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            title: const Text('Developer Info'),
-            children: <Widget>[
-              SimpleDialogOption(
-                onPressed: () {
-                  Navigator.pop(context, DeveloperInfo.frontEnd);
-                },
-                child: const Text('Front End')
-              ),
-              SimpleDialogOption(
-                onPressed: () {
-                  Navigator.pop(context, DeveloperInfo.backEnd);
-                },
-                child: const Text('Back End')
-              )
-            ],
-          );
-        }
-      )) {
-      case DeveloperInfo.frontEnd:
-        print('Eason Lu, Alexa Hu');
-        break;
-      case DeveloperInfo.backEnd:
-        print('Chenhui Zhang, Yihong Jian');
-        break;
+  //void developerInfoDialog() {
+  Future<void> openDialog() async {
+    switch (await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Developer Info'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, DeveloperInfo.frontEnd);
+              },
+              child: const Text('Front End')
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, DeveloperInfo.backEnd);
+              },
+              child: const Text('Back End')
+            )
+          ],
+        );
       }
+    )) {
+    case DeveloperInfo.frontEnd:
+      print('Eason Lu, Alexa Hu');
+      break;
+    case DeveloperInfo.backEnd:
+      print('Chenhui Zhang, Yihong Jian');
+      break;
     }
   }
 }
