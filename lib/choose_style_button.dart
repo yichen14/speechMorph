3b38e1a85dce0fccea888cf33f086e7fdf6155a0
xuningh2 <xuningh2@illinois.edu>
2019-12-07 19:59:36 -0600

    choose style button second try

--- lib/choose_style_button.dart
+++ lib/choose_style_button.dart
@@ -22,7 +22,6 @@ class ChooseStyleButtonState extends State<ChooseStyleButtonWidget> {
             Text('Speech Style',
             style: TextStyle(fontSize: 30.0)
             ),
-            getListView(),
             PopupMenuButton<ChoiceOfSpeechStyle> (
               initialValue: choices[0],
               onCanceled: () => print("You didn't choose anything"),
@@ -36,55 +35,32 @@ class ChooseStyleButtonState extends State<ChooseStyleButtonWidget> {
                 }).toList();
               },
             ),
-            /*
-            FlatButton(
-              color: Colors.white,
-              textColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-              onPressed: () {
-                _onPressed();
-              },
-              child: Text("Speech Style",
-                style: TextStyle(fontSize: 30.0),
-              ),
-            ),
-            */
+           //getListView(),
           ],
         ),
       ],
     );
   }
-
+  /*
   Widget getListView() {
     var listView = ListView (
       children: <Widget> [
         ListTile(
           leading: Icon(Icons.person),
-          title: Text("Speech Style"),
-          onTap: () {
-            _onPressed();
-          }
-        ),
-        ListTile(
-          leading: Icon(Icons.person),
           title: Text("Developer Info"),
-        )
-      ]
+          onTap: () {
+            return showDialog(context: context, builder: (context) {
+              return AlertDialog(
+                content: Text('Front end: Eason Lu, Alexa Hu')
+              );
+            });
+          },
+        ),
+      ],
     );
+    return listView;
   }
-
-  void _onPressed() {
-    setState(() {
-      itemBuilder: (BuildContext context) {
-        return choices.map((ChoiceOfSpeechStyle choice) {
-          return new PopupMenuItem<ChoiceOfSpeechStyle>(
-            value: choice,
-            child: new Text(choice.title),
-          );
-        }).toList();
-      };
-    });
-  }
+  */
 
   void choiceAction(ChoiceOfSpeechStyle choice) {
     setState(() {
@@ -100,6 +76,6 @@ class ChoiceOfSpeechStyle {
 
 List<ChoiceOfSpeechStyle> choices = <ChoiceOfSpeechStyle> [
   new ChoiceOfSpeechStyle(title: 'Trump'),
-  //new ChoiceOfSpeechStyle(title: '')
+  new ChoiceOfSpeechStyle(title: 'Hillary'),
 ];
 
